fc06dd73c4865148baccdfb3e9423a8342f8e0b7
David Hall <david.hall.physics@gmail.com>
2020-02-15 22:49:12 -0800

    Fix test Example used external packages

--- R/classify_version.R
+++ R/classify_version.R
@@ -2,16 +2,13 @@
 #'
 #' @param x An `ip_address` or `ip_network` vector
 #'
-#' @examples
-#' tibble(ip = ip_address(c("0.0.0.1", "192.168.0.1", "2001:db8::7334"))) %>%
-#'   mutate(ipv4 = is_ipv4(ip), ipv6 = is_ipv6(ip))
-#'
 #' @name classify_version
 NULL
 
 #' `is_ipv4`
-#'
 #' `is_ipv4` identifies IPv4 addresses/networks
+#' @examples
+#' is_ipv4(ip_address(c("0.0.0.1", "192.168.0.1", "2001:db8::7334")))
 #'
 #' @rdname classify_version
 #' @export
@@ -20,8 +17,9 @@ is_ipv4 <- function(x) {
 }
 
 #' `is_ipv6`
-#'
 #' `is_ipv6` identifies IPv6 addresses/networks
+#' @examples
+#' is_ipv6(ip_address(c("0.0.0.1", "192.168.0.1", "2001:db8::7334")))
 #'
 #' @rdname classify_version
 #' @export
