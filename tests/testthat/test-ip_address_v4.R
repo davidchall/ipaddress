542e261b10159e3acf541e45fd055e74e1657685
David Hall <david.hall.physics@gmail.com>
2020-03-04 17:29:46 -0800

    Encode NAs in logical field (is_ipv6) NA_integer_ encodes a meaningful address so we can't use this But the logical NA is unambiguous

--- tests/testthat/test-ip_address_v4.R
+++ tests/testthat/test-ip_address_v4.R
@@ -33,7 +33,7 @@ test_that("coercion works", {
 })
 
 test_that("missing values work", {
-  expect_equal(field(ip_address(NA), "address1"), NA_integer_)
+  expect_equal(field(ip_address(NA), "is_ipv6"), NA)
   expect_equal(ip_address(c(x, NA)), c(ip_address(x), NA))
   expect_equal(as.character(ip_address(c(x, NA))), c(x, NA))
   expect_equal(is.na(ip_address(c(x, NA))), c(rep(FALSE, length(x)), TRUE))
