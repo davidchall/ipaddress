3b2039d34cfcd896a7aafbf3c11b7f4aedea8879
David Hall <david.hall.physics@gmail.com>
2020-03-21 18:20:21 -0700

    More tests

--- tests/testthat/test-hostname.R
+++ tests/testthat/test-hostname.R
@@ -1,13 +1,11 @@
-test_that("DNS resolution works", {
+test_that("returns expected type and size", {
   skip_if_offline()
 
-  # expected type
   expect_s3_class(from_hostname("localhost", multiple = FALSE), "ip_address")
   expect_type(from_hostname("localhost", multiple = TRUE), "list")
   expect_type(as_hostname(ip_address("::1"), multiple = FALSE), "character")
   expect_type(as_hostname(ip_address("::1"), multiple = TRUE), "list")
 
-  # expected size
   expect_length(from_hostname("localhost", multiple = FALSE), 1)
   expect_length(from_hostname("localhost", multiple = TRUE), 1)
   expect_length(from_hostname(c("localhost", "www.r-project.org"), multiple = FALSE), 2)
@@ -17,8 +15,23 @@ test_that("DNS resolution works", {
   expect_length(as_hostname(ip_address("::1"), multiple = TRUE), 1)
   expect_length(as_hostname(ip_address(c("::1", "::2")), multiple = FALSE), 2)
   expect_length(as_hostname(ip_address(c("::1", "::2")), multiple = TRUE), 2)
+})
 
-  # input validation
+test_that("input validation works", {
+  expect_error(as_hostname(ip_network("192.168.0.0/24")))
+  expect_error(as_hostname("127.0.0.1"))
+  expect_error(as_hostname(ip_address("127.0.0.1"), multiple = "yes"))
+  expect_error(as_hostname(ip_address("127.0.0.1"), multiple = NA))
+
+  expect_error(from_hostname(123))
+  expect_error(from_hostname(ip_address("127.0.0.1")))
+  expect_error(from_hostname("localhost", multiple = "yes"))
+  expect_error(from_hostname("localhost", multiple = NA))
+})
+
+test_that("DNS resolution works", {
+  skip_if_offline()
+
   expect_warning(from_hostname("unknown"))
 
   # localhost is a reserved top-level domain (suitable for unit testing?)
@@ -27,7 +40,15 @@ test_that("DNS resolution works", {
   expect_match(as_hostname(ip_address("::1")), "localhost")
 })
 
-test_that("internal pluck_first_of_each() function works", {
+test_that("missing values work", {
+  skip_if_offline()
+
+  expect_equal(from_hostname(NA_character_), ip_address(NA))
+  expect_equal(as_hostname(ip_address(NA), multiple = FALSE), NA_character_)
+  expect_equal(as_hostname(ip_address(NA), multiple = TRUE), list(NA_character_))
+})
+
+test_that("internal function pluck_first_of_each() works", {
   # works with character vectors
   expect_equal(
     pluck_first_of_each(list("a")),
