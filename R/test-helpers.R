8735bd98550f86f479f90552895ee2e802e1e1ae
David C Hall <davidchall@users.noreply.github.com>
2020-08-08 11:57:07 -0700

    Refactor how reserved networks are tested (#48)

---
+++ R/test-helpers.R
@@ -0,0 +1,79 @@
+#' Expectation: does the function identify the reserved address range?
+#'
+#' This tests the addresses either side of the reserved network boundaries.
+#'
+#' @param reserved_func The function under test. This should return `TRUE` when
+#'   an [`ip_address`] is within the reserved network.
+#' @param network The reserved network
+#' @noRd
+expect_reserved_address_range <- function(reserved_func, network) {
+  act <- quasi_label(enquo(reserved_func), arg = "reserved_func")
+
+  block_start <- network_address(network)
+  block_end <- broadcast_address(network)
+  space_start <- ip_address(c("0.0.0.0", "::"))
+  space_end <- ip_address(c("255.255.255.255", "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"))
+
+  boundaries <- vec_c(
+    if (all(block_start != space_start)) block_start - 1,
+    block_start,
+    block_end,
+    if (all(block_end != space_end)) block_end + 1
+  )
+
+  expected <- c(
+    if (all(block_start != space_start)) FALSE,
+    TRUE,
+    TRUE,
+    if (all(block_end != space_end)) FALSE
+  )
+
+  expect(
+    all(act$val(boundaries) == expected),
+    sprintf("%s doesn't match address range '%s'", act$lab, as.character(network))
+  )
+
+  invisible(act$val)
+}
+
+#' Expectation: does the function identify the reserved network?
+#'
+#' This tests the subnets and supernet of the reserved network.
+#'
+#' @param reserved_func The function under test. This should return `TRUE` when
+#'   an [`ip_network`] is within the reserved network.
+#' @param network The reserved network
+#' @noRd
+expect_reserved_network <- function(reserved_func, network) {
+  act <- quasi_label(enquo(reserved_func), arg = "reserved_func")
+
+  other_networks <- vec_c(
+    network,
+    if (prefix_length(network) != max_prefix_length(network)) subnets(network),
+    if (prefix_length(network) != 0) supernet(network)
+  )
+  expected <- c(
+    TRUE,
+    if (prefix_length(network) != max_prefix_length(network)) c(TRUE, TRUE),
+    if (prefix_length(network) != 0) FALSE
+  )
+
+  expect(
+    all(act$val(other_networks) == expected),
+    sprintf("%s doesn't match network '%s'", act$lab, as.character(network))
+  )
+
+  invisible(act$val)
+}
+
+#' Expectation: does the function identify reserved addresses and networks?
+#'
+#' @param reserved_func The function under test. This should return `TRUE` when
+#'   an [`ip_address`] or [`ip_network`] is within the reserved network.
+#' @param network The reserved network
+#' @noRd
+expect_reserved <- function(reserved_func, network) {
+  func <- enquo(reserved_func)
+  expect_reserved_address_range(!!func, network)
+  expect_reserved_network(!!func, network)
+}
