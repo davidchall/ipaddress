3cb8763e49a7e2cb2300e9f409e1f796eb415405
David C Hall <davidchall@users.noreply.github.com>
2023-11-30 13:21:20 -0800

    Fix logic

--- src/asio_config.h
+++ src/asio_config.h
@@ -2,7 +2,7 @@
 #define __IPADDRESS_ASIO_CONFIG__
 
 // libc++ throws bogus warning about allocator<void> -- https://llvm.org/PR50299
-#if defined(_LIBCPP_VERSION) && (_LIBCPP_VERSION >= 13000) && defined(_LIBCPP_STD_VER) && (_LIBCPP_STD_VER == 17)
+#if defined(_LIBCPP_VERSION) && (_LIBCPP_VERSION < 13000) && defined(_LIBCPP_STD_VER) && (_LIBCPP_STD_VER == 17)
   #define _LIBCPP_DISABLE_DEPRECATION_WARNINGS
 #endif
 
